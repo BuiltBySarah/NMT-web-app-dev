e79a8a83e77d551352ac68eb5dad10ed1af963c6
Sarah <20084807@tafe.wa.edu.au>
2025-08-05 14:46:41 +0800

    Completed Session 2 Challenge 2

--- session-02/assets/js_starter/scripts_challenge_2.js
+++ session-02/assets/js_starter/scripts_challenge_2.js
@@ -4,9 +4,13 @@ let array1 = [1, 2, 3, 2];
 let array2 = [4, 5, 6, 4];
 
 // Use the spread operator to merge the two arrays
+const mergedArray = [...array1, ...array2];
 
 // Use the Set object to remove duplicates
+new Set(mergedArray);
 
 // Use array destructuring to assign the first three elements to variables
+const [first, second, third] = mergedArray;
 
 // Set the value of the output filed to be the second element of the unique array
+document.getElementById("OutputField").innerHTML = second;
