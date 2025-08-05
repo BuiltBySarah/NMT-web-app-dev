1f304936b13b8c647245d09a9914e428550af2ce
Sarah <20084807@tafe.wa.edu.au>
2025-08-05 21:21:36 +0800

    Completed Session 3 Challenge 3

--- session-03/assets/js_starter/scripts_challenge_3.js
+++ session-03/assets/js_starter/scripts_challenge_3.js
@@ -1,10 +1,15 @@
 // Challenge 3 Starter
 // Create the class and use the constructor method
 class UserObject {
-
+    constructor (fNameInput, lNameInput, ageInput) {
+        this.firstName = fNameInput
+        this.lastName = lNameInput
+        this.age = ageInput
+    }
 }
 
 // Create an instance of the ComplexObject class (hint: add the properties)
-const userObjectInstance = {}
+const userObjectInstance = new UserObject("Sarah", "Aristei", 19);
 
 // Log the new instance of the object
+console.log(userObjectInstance);
