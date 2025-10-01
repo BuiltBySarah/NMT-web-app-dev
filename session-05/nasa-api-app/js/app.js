b2dcda285dca14579cf2c9513d8165c8b9d97a35
Sarah <20084807@tafe.wa.edu.au>
2025-10-01 19:30:46 +0800

    Completed Session 5 challenges

--- session-05/nasa-api-app/js/app.js
+++ session-05/nasa-api-app/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fetchApodButton = document.getElementById("fetch-apod");
   const apodContent = document.getElementById("apod-content");
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "oX03fJ44j7dn1iOpgcwVBOmzmXHFRVTgeozm5S30";
 
   fetchApodButton.addEventListener("click", () => {
     const date = document.getElementById("date").value;
@@ -17,78 +17,94 @@ document.addEventListener("DOMContentLoaded", () => {
     // 2. add them to the apiURL as parameters
     // 3. Test the responses in the Network tab
 
+    if (date) {
+      apiUrl += `&date=${date}`;
+    } else if (startDate) {
+      apiUrl += `&start_date=${startDate}`;
+    } else if (endDate) {
+      apiUrl += `&end_date=${endDate}`;
+    } else if (count) {
+      apiUrl += `&count=${count}`;
+    } else if (thumbs) {
+      apiUrl += `&thumbs=${thumbs}`;
+    }
+  
     // Challenge 2
     // 1. add the following headers to your API call content type, user agent & cache control
+    let headersList = {
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
     // 2. set the cache control no no cache
+    "Cache-Control": "no-cache",
     // 3. set the content type application/json
+    "Content-Type": "application/json"
     // 4. Test the responses in the Network tab
-
+    }
+    
     fetch(apiUrl, {
       method: "GET",
+      headers: headersList
     })
       // Challenge 3
       // 1. change the anonymous arrow function below to check if the response code is 200(ok)
+    .then((response) => {
       // 2. if the response is ok return the response.json() object
       // 3. if not ok throw a new error which includes the status code
       // 4. Test the responses in the Network tab
-      .then((response) => {
+      if (response.ok) {
+        console.log(response);
         return response.json();
-      })
-      .then((data) => {
-        if (Array.isArray(data)) {
-          apodContent.innerHTML = "";
-          data.forEach((item) => {
-            if (item.media_type === "image") {
-              apodContent.innerHTML += `
-                              <figure class="image is-4by3">
-                                  <img src="${item.url}" alt="${item.title}">
-                              </figure>
-                              <h2 class="title is-4">${item.title}</h2>
-                              <p>${item.explanation}</p>
-                          `;
-            } else if (item.media_type === "video") {
-              apodContent.innerHTML += `
-                              <div class="video-apodContent">
-                                  <iframe src="${item.url}" frameborder="0" allowfullscreen></iframe>
-                              </div>
-                              <h2 class="title is-4">${item.title}</h2>
-                              <p>${item.explanation}</p>
-                          `;
-            } else {
-              apodContent.innerHTML += `<p>Media type not supported: ${item.media_type}</p>`;
-            }
-          });
-        } else {
-          apodContent.innerHTML = "";
-          if (item.media_type === "image") {
-            apodContent.innerHTML += `
-                            <figure class="image is-4by3">
-                                <img src="${item.url}" alt="${item.title}">
-                            </figure>
-                            <h2 class="title is-4">${item.title}</h2>
-                            <p>${item.explanation}</p>
-                        `;
-          } else if (item.media_type === "video") {
-            apodContent.innerHTML += `
-                            <div class="video-apodContent">
-                                <iframe src="${item.url}" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                            <h2 class="title is-4">${item.title}</h2>
-                            <p>${item.explanation}</p>
-                        `;
-          } else {
-            apodContent.innerHTML += `<p>Media type not supported: ${item.media_type}</p>`;
-          }
-        }
-      })
-      .catch((error) => {
-        apodContent.innerHTML = `<p>Error fetching item: ${error.message}</p>`;
-      });
-  });
+      } else {
+        throw new Error(`${console.log(response.status)}`)
+      }
+    })
+    
+    .then((data) => {
+      apodContent.innerHTML = "";
 
-  // // Challenge 4
-  // function displayApod(data, apodContent) {
-  //   // Challenge 4
-  //   // 1. Move the if/else if/else into a resusable function ready for modularisation
-  // }
+      // Challenge 4
+      // 1. Move the if/else if/else into a resusable function ready for modularisation
+
+      function displayApod(data, apodContent) {
+        if (data.media_type === "image") {
+          return apodContent.innerHTML += `
+                            <figure class="image is-4by3">
+                                <img src="${data.url}" alt="${data.title}">
+                            </figure>
+                            <h2 class="title is-4">${data.title}</h2>
+                            <p>${data.explanation}</p>
+                        `;
+        } else if (data.media_type === "video") {
+          return apodContent.innerHTML += `
+                               <div class="video-apodContent">
+                                   <iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>
+                               </div>
+                               <h2 class="title is-4">${data.title}</h2>
+                               <p>${data.explanation}</p>
+                           `;
+        } else {
+          return apodContent.innerHTML += `<p>Media type not supported: ${data.media_type}</p>`;
+        }
+      }
+
+      if (Array.isArray(data)) {
+        for (let counter = 0; counter < data.length; counter++) {
+          displayApod(data[counter], apodContent)
+        }
+      } else {
+        displayApod(data, data.media_type)
+        console.log(displayApod(data, apodContent))
+      };
+    })
+    
+    .catch((error) => {
+      apodContent.innerHTML = `<p>Error fetching item: ${error.message}</p>`;
+    });
+  });
 });
+
+
+
+
+
+
+
