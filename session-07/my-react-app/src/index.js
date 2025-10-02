ef2a33c8a26a5294247e511534be9bb2ff9c4559
Sarah <20084807@tafe.wa.edu.au>
2025-10-02 11:16:30 +0800

    React setup and completed Session 7 Challenge 1

--- session-07/my-react-app/src/index.js
+++ session-07/my-react-app/src/index.js
@@ -1,3 +1,11 @@
+//Problems I had (in case React decides to stop working again):
+// Killing the current process -
+// netstat -ano | findstr :3000
+// The last number is the ID
+// TCP    0.0.0.0:3000    0.0.0.0:0    LISTENING    12345
+// taskkill /PID 12345 /F
+// Re-run npm start on Command prompt
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 
