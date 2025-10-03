006ec9c4608452dc6c7a167c031d29f620deb43f
Sarah <20084807@tafe.wa.edu.au>
2025-10-03 15:39:14 +0800

    Completed Session 8 Challenge 8

--- session-08/in-class-code/my-react-app/src/utils/utils.js
+++ session-08/in-class-code/my-react-app/src/utils/utils.js
@@ -1,9 +1,9 @@
-  // Use getUserInfo utility function to generate user info string
-  // Getuserinfo function
-  // formatSkills function
-    // Step 3 fix the map method & fix duplicated method
-    // const formatSkills = function(skills) {
-    //   return skills.map(() => skill.touppercase());
-    // };
-    //const formattedSkills = formatSkills(user.skills);
+export const getUserInfo = function(object) {
+  return `Name: ${object.firstName} ${object.lastName}, Age: ${object.age}`;
+}
+
+export const formatUserSkills = function(object) {
+  return object.map((skill) => skill.toUpperCase());
+}
+
   
